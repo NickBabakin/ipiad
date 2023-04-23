a5ce43cf59b09a6e3aae350b5e288d6d6f6ea127
NickBabakin <NickBabakin@yandex.ru>
2023-04-23 13:19:37 +0300

    parsing starting page complete

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	p "github.com/NickBabakin/ipiad/parser"
 )
 
 func main() {
 	parser := p.Parser{}
-	html := parser.HTMLfromURL("https://career.habr.com/vacancies")
-	fmt.Printf("\n\n\n\n NOW TO LINK \n\n\n\n")
-	parser.ParseHTML(html)
+	doc := parser.HTMLfromURL("https://career.habr.com/vacancies")
+	parser.ParseStartingHTML(doc)
 }
--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -13,49 +14,49 @@ import (
 type Parser struct{}
 
 func getVacanciesURLs(n *html.Node) []string {
-	var fillURLs func(*html.Node, *[]string)
-	fillURLs = func(n *html.Node, vacanciesURLs *[]string) {
+	var fillURLs func(*html.Node, *[]string, *regexp.Regexp)
+	re, _ := regexp.Compile(`^\/vacancies\/[0-9]+$`)
+	fillURLs = func(n *html.Node, vacanciesURLs *[]string, re *regexp.Regexp) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					//fmt.Println(a.Val)
-					*vacanciesURLs = append(*vacanciesURLs, a.Val)
-					break
+					if re.MatchString(a.Val) {
+						*vacanciesURLs = append(*vacanciesURLs, "https://career.habr.com"+a.Val) //TODO: append unique only
+					}
 				}
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			fillURLs(c, vacanciesURLs)
+			fillURLs(c, vacanciesURLs, re)
 		}
-		//fmt.Println(vacanciesURLs)
 	}
 	vacanciesURLs := make([]string, 0, 512)
-	fillURLs(n, &vacanciesURLs)
+	fillURLs(n, &vacanciesURLs, re)
 	return vacanciesURLs
 }
 
-func (p Parser) HTMLfromURL(url string) string {
-	res, err := http.Get(url)
+func (p Parser) HTMLfromURL(url string) *html.Node {
+	res, err := http.Get(url) //TODO: check for http error codes
 	if err != nil {
 		log.Fatal(err)
 	}
 	html_data, err := io.ReadAll(res.Body)
-	html := string(html_data)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
-	return html
-}
-
-func (p Parser) ParseHTML(html_string string) {
+	html_string := string(html_data)
 	doc, err := html.Parse(strings.NewReader(html_string))
 	if err != nil {
 		log.Fatal(err)
 	}
+	return doc
+}
+
+func (p Parser) ParseStartingHTML(doc *html.Node) {
 	vacanciesURLs := getVacanciesURLs(doc)
 	for _, s := range vacanciesURLs {
 		fmt.Println(s)
 	}
-	fmt.Println(len(vacanciesURLs), "  ", cap(vacanciesURLs))
+	fmt.Println(len(vacanciesURLs))
 }
