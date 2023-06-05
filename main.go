5ee171a2562195f2fe9a8bf15494336e65ab71c5
NickBabakin <NickBabakin@yandex.ru>
2023-06-05 21:13:33 +0300

    logs to file added

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
 	"sync"
 
 	p "github.com/NickBabakin/ipiad/parser"
 )
 
 func main() {
+
+	logFile, err := os.OpenFile("log.txt", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
 
 	var wg sync.WaitGroup
 	wg.Add(3)
@@ -16,4 +26,5 @@ func main() {
 	go p.SaveVacancies(&wg)
 
 	wg.Wait()
+
 }
